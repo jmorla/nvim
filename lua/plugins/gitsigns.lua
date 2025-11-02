e5e9df3acd2fb75a1edceb0544802a2ad67d4c65
Jorge Morla <jorgemorlapro@gmail.com>
2025-11-02 17:37:29 -0400

    keymapping for gitsigns

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -38,6 +38,39 @@ return {
         row = 0,
         col = 1,
       },
+      on_attach = function(bufnr)
+        local gitsigns = require("gitsigns")
+        local function map(mode, l, r, opts)
+          opts = opts or {}
+          opts.buffer = bufnr
+          vim.keymap.set(mode, l, r, opts)
+        end
+
+        -- Navigation
+        map("n", "]c", function()
+          if vim.wo.diff then return "]c" end
+          vim.schedule(function() gitsigns.next_hunk() end)
+          return "<Ignore>"
+        end, { expr = true })
+        map("n", "[c", function()
+          if vim.wo.diff then return "[c" end
+          vim.schedule(function() gitsigns.prev_hunk() end)
+          return "<Ignore>"
+        end, { expr = true })
+
+        -- Actions
+        map("n", "<leader>hs", function() gitsigns.stage_hunk() end)
+        map("n", "<leader>hr", function() gitsigns.reset_hunk() end)
+        map("n", "<leader>hp", function() gitsigns.preview_hunk() end)
+        map("n", "<leader>hb", function() gitsigns.blame_line() end)
+        map("n", "<leader>hS", function() gitsigns.stage_buffer() end)
+        map("n", "<leader>hR", function() gitsigns.reset_buffer() end)
+        map("n", "<leader>hu", function() gitsigns.undo_stage_hunk() end)
+        map("n", "<leader>hd", function() gitsigns.diffthis() end)
+
+        -- Text objects
+        map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>")
+      end,
     })
   end,
 }
